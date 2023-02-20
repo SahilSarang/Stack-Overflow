cc0a485d47174bb22db3104f95f82365c997b514
SahilSarang <125231810+SahilSarang@users.noreply.github.com>
2023-02-20 19:26:49 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,12 +9,11 @@ export const signup = async(req, res) =>{
       const existinguser = await users.findOne({email});
         if(existinguser){
             return res.status(404).json({message: "User already exist."})
-     }
-
+     }else{
         const hashPassword = await bcrypt.hash(password, 12)
         const newUser = await users.create({name, email, password: hashPassword})
         const token = jwt.sign({email: newUser.email, id:newUser._id}, process.env.JWT_TOKEN, {expiresIn: '1h'})
-        res.status(200).json({result: newUser, token})
+        res.status(200).json({result: newUser, token})}
     } catch(error){
        res.status(500).json("Something went wrong....")
     }
@@ -26,14 +25,13 @@ export const login = async(req, res) =>{
     const existinguser = await users.findOne({email});
     if(!existinguser){
       return res.status(404).json({message: "User dosen't exist."})
-    }
-
+    }else{
     const isPasswordCrt = await bcrypt.compare(password, existinguser.password)
     if(!isPasswordCrt){
       return res.status(400).json({message: "Invalid Credentials"})
     }
       const token = jwt.sign({email: existinguser.email, id:existinguser._id}, process.env.JWT_TOKEN, {expiresIn: '1h'})
-      res.status(200).json({result: existinguser, token})
+      res.status(200).json({result: existinguser, token})}
   }catch(error) {
     res.status(500).json("Something went wrong....")
   }
