676d96cfb264da3c98bcc56b56fd36c13c521501
SahilSarang <125231810+SahilSarang@users.noreply.github.com>
2023-02-21 21:19:42 +0530

    Update QuestionDetails.jsx

--- client/src/Pages/Questions/QuestionDetails.jsx
+++ client/src/Pages/Questions/QuestionDetails.jsx
@@ -80,7 +80,7 @@ const QuestionsDetails = () => {
   const dispatch = useDispatch()
   const User = useSelector((state) => (state.currentUserReducer))
   const location = useLocation()
-  const url = 'http://localhost:3000'
+  const url = 'https://stack-overflow-ss.netlify.app/Questions'
   
   const handlePostAns = (e, answerLength) =>{
     e.preventDefault()
