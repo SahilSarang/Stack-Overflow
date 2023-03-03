e4475ae516797b3b6ca8e1c56161313d08e46408
SahilSarang <125231810+SahilSarang@users.noreply.github.com>
2023-03-03 17:12:41 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,11 +9,12 @@ export const signup = async(req, res) =>{
       const existinguser = await users.findOne({email});
         if(existinguser){
             return res.status(404).json({message: "User already exist."})
-     }else{
-        const hashPassword = await bcrypt.hash(password, 12)
-        const newUser = await users.create({name, email, password: hashPassword})
-        const token = jwt.sign({email: newUser.email, id:newUser._id}, process.env.JWT_TOKEN, {expiresIn: '1h'})
-        res.status(200).json({result: newUser, token})}
+        }else{
+            const hashPassword = await bcrypt.hash(password, 12)
+            const newUser = await users.create({name, email, password: hashPassword})
+            const token = jwt.sign({email: newUser.email, id:newUser._id}, process.env.JWT_TOKEN, {expiresIn: '1h'})
+            res.status(200).json({result: newUser, token})
+        }
     } catch(error){
        res.status(500).json("Something went wrong....")
     }
