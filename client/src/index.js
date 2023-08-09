0249d49baa7c60656447342a7dacef746bef34cb
AmezingHacker <125231810+SahilSarang@users.noreply.github.com>
2023-08-09 21:21:55 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,17 +7,10 @@ import { configureStore } from '@reduxjs/toolkit'
 //import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import Reducers from './reducers'
-import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
 //const store = configureStore({reducer: Reducers, middleware: (getDefaultMiddleware) => getDefaultMiddleware(thunk)})
 const store = configureStore({reducer: Reducers, middleware: [thunk]})
 //const store = createStore( Reducers, compose(applyMiddleware(thunk)))
-
-Kommunicate.init("", {
-  automaticChatOpenOnNavigation: true,
-  popupWidget: true
-});
-
 
 ReactDOM.render(
   <Provider store={store}>
